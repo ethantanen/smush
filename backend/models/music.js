e723f18eb422b5470c7eff26fae58bb38d008a0e
ethantanen <e_tanen@coloradocollege.edu>
2018-11-28 22:54:40 -0700

    added user, music entry databases/ model

--- backend/models/music.js
+++ backend/models/music.js
@@ -1,15 +1,13 @@
-const grid = require('gridfs-stream')
-const mongo = require('mongodb')
-const upload = require('multer')({dest:'upload/'})
-const path = require('path')
-const uuid =
+// TODO: check for redundency before inserting a document
+var mongoose = require('mongoose');
 
 // NOTE: we can limit amount of data by making the midi file a derived element
-function make_entry(artist_name, track_name, music_xml, key, tempo, image) {
+// return json representation of music database entry
+function makeEntry(artistName, trackName, musicXML, key, tempo, image) {
   return {
-    artist_name: artist_name,
-    track_name: track_name,
-    music_xml: music_xml,
+    artistName: artistName,
+    trackName: trackName,
+    musicXML: musicXML,
     key: key,
     tempo: tempo,
     image: image,
@@ -17,36 +15,66 @@ function make_entry(artist_name, track_name, music_xml, key, tempo, image) {
 }
 
 // insert document into database
-function insert(artist_name=null, track_name=null, music_xml=null, key=null, tempo=null, image=null) {
-  entry = make_entry(artist_name, track_name, music_xml, key, tempo, image)
-
+function insert(artistName=null, trackName=null, musicXML=null, key=null, tempo=null, image=null) {
+  return new Promise((resolve, reject) => {
+    entry_json = makeEntry(artistName, trackName, musicXML, key, tempo, image)
+    entry = new Music(entry_json)
+    entry.save().then((entry) => {
+      if(!entry) return reject('couldn\'t add user')
+      return resolve(entry)
+    })
+  })
 }
 
-// query database
-function select() {
-  return
+// select document from database, returns a promise-like object (good for async, await)
+// TODO: maybe create specific functions for more
+// complex queries
+function select(query) {
+  return new Promise((resolve, reject) => {
+    Music.find(query).then((entry) => {//.limit.sort ... very open ended!
+      if (entry) return reject(entry)
+      return resolve(entry)
+    })
+  })
 }
 
 // update document in database
-function update() {
-  return
+function update(query, update) {
+  return new Promise((resolve, reject) => {
+    Music.findOneAndUpdate(query, update).then((entry) => {
+      if (!entry) return reject('user doesnt exist')
+      return resolve(entry)
+    })
+  })
 }
 
-// delete document from
-function delete() {
-  return 
+// delete document from database
+function remove(query) {
+  return new Promise((resolve, reject) => {
+    Music.find(query).deleteOne().then((meta) => {
+      if (!meta) return reject('entry doesn\'t exist')
+      return resolve(meta) //TODO: what does remove return?
+    })
+  })
 }
 
-// convert images to base64
-function base64Encode(file) {
-  return new Buffer(file).toString('base64')
-}
+// connect to database
+async function connect() {
+  // connect to mongo database
+  const URI = 'mongodb://' + process.env.USERNAME + ':' + process.env.PASSWORD +
+    '@ds115712.mlab.com:15712/music_app_1234'
+  mongoose.connect(URI, {useNewUrlParser: true})
 
-// connect to mongo database
-const URI = 'mongodb://' + process.env.USERNAME + ':' + process.env.PASSWORD + \
-  '@ds115712.mlab.com:15712/music_app_1234'
-mongo.MongoClient.connect(URI, async (err, db) => {
-  if (err) return console.log(err)
-  gfs = grid(db,mongo)
-  console.log('connected to db')
-})
+  // define user schema
+  userSchema = new mongoose.Schema({
+    artistName: String,
+    trackName: String,
+    musicXML: String,
+    key: String,
+    tempo: Number,
+    image: String
+  })
+
+  // create model
+  global.Music = mongoose.model('Music', userSchema)
+}
--- backend/models/users.js
+++ backend/models/users.js
@@ -1,43 +1,77 @@
+// NOTE: need to do some research on passport
+// for authentication and such
+// TODO: check for reduncy before inserting a document
 
-// NOTE: need to do some research on passport for authentication and such
+var mongoose = require('mongoose');
 
-const grid = require('gridfs-stream')
-const mongo = require('mongodb')
-const upload = require('multer')({dest:'upload/'})
-const path = require('path')
-
-// NOTE: we can limit amount of data by making the midi file a derived element
-function make_entry(username) {
+// return json representation of user
+function makeUser(username=null) {
   return {
-    username: username
+    username: username,
   }
 }
 
 // insert document into database
 function insert(username=null) {
-  entry = make_entry(username)
+  return new Promise((resolve, reject) => {
+    user_json = makeUser(username)
+    user = new User(user_json)
+    user.save().then((user) => {
+      if(!user) return reject('couldn\'t add user')
+      return resolve(user)
+    })
+  })
 }
 
-// select document from database
-function select() {
-  return
+// select document from database, returns a promise-like object (good for async, await)
+// TODO: maybe create specific functions for more
+// complex queries
+function select(query) {
+  return new Promise((resolve, reject) => {
+    User.find(query).then((users) => {//.limit.sort ... very open ended!
+      if (!users) return reject('couldn\'t select user')
+      return resolve(users)
+    })
+  })
 }
 
 // update document in database
-function update() {
-  return
+function update(query, update) {
+  return new Promise((resolve, reject) => {
+    User.findOneAndUpdate(query, update).then((user) => {
+      if (!user) return reject('couldn\'t update user')
+      return resolve(user)
+    })
+  })
 }
 
-// delete document from database 
-function delete() {
-  return
+// delete document from database
+function remove(query) {
+  return new Promise((resolve, reject) => {
+    User.find(query).deleteOne().then((meta) => {
+      if (!meta) return reject('couldn\'t remove user')
+      return resolve(meta) //TODO: what does remove return?
+    })
+  })
 }
 
-// connect to mongo database
-const URI = 'mongodb://' + process.env.USERNAME + ':' + process.env.PASSWORD + \
-  'add name of database woo!'
-mongo.MongoClient.connect(URI, async (err, db) => {
-  if (err) return console.log(err)
-  gfs = grid(db,mongo)
-  console.log('connected to db')
-})
+// connect to database
+async function connect() {
+  // connect to mongo database
+  const URI = 'mongodb://' + process.env.USERNAME + ':' + process.env.PASSWORD +
+    '@ds115712.mlab.com:15712/music_app_1234'
+  mongoose.connect(URI, {useNewUrlParser: true})
+
+  // define user schema
+  userSchema = new mongoose.Schema({
+    username: String
+  })
+
+  // create model
+  global.User = mongoose.model('Users', userSchema)
+
+  //TODO: this is the place to attatch functions to the model
+  remove({'username': 'adfaj;'})
+    .then(console.log)
+    .catch(console.log)
+}
