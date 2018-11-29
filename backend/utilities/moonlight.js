93d12776e9b35c951162e8ac6ecd39c552b9399a
ethantanen <e_tanen@coloradocollege.edu>
2018-11-29 11:47:49 -0700

    couple notes on the moonlight utility file

---
+++ backend/utilities/moonlight.js
@@ -0,0 +1,3 @@
+// have function that takes base64 encoded image and returns musicXML
+// moonlight should have a python api but theres a chance we'll need to
+// execute bash commands from the node environment
