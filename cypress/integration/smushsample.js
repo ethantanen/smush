abbaa4e0e3f2bffcb61732ad3df9f22e3c80a8f0
Quentin Kakuda <qkakuda@yahoo.com>
2018-12-11 11:02:31 -0700

    removed old spec tests

--- cypress/integration/smushsample.js
+++
@@ -1,7 +0,0 @@
-// use cy.visit to go to a url
-describe('Basic ass tests', function() {
-  it('Checks home page title', function() {
-    cy.visit('http://localhost:3000')
-    cy.contains('SMUSH')
-  })
-})
